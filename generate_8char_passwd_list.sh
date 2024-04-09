f4e6dac9a8af92cb978510457a187771d1d981d4
hlr0 <160110248+hlr0@users.noreply.github.com>
2024-04-09 19:40:47 +0200

    Update generate_8char_passwd_list.sh

--- generate_8char_passwd_list.sh
+++ generate_8char_passwd_list.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#USAGE: parallel -I% -n1 -P2 "./generate_8char_passwd_list.sh %"
+
+#Description
+# Make sure GNU parellel is installed on your system before using this file.
 
 # declare an array called array and define values
 array=( a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 0 '!' '@' '#' '$' '%' '^' '&' '*' '(' ')' '_' '-' '=' '+' '`' '~' '{' '}' '|' ':' '"' '<' '>' '?' '[' ']' '\' ';' "'" ',' '.' '/' )
